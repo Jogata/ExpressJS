105d0af49b716913904d2663a36ead480583e31f
Yonko Nikolov <jonkoslav@abv.bg>
2022-01-27 13:10:23 +0200

    change HTTP Server with Express Server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,32 +1,10 @@
-// Add index.html and css
-const http = require("http");
-const {readFileSync} = require("fs");
+// change HTTPserver with Express
 
-const homePage = readFileSync('./methods-public/index.html');
-const homeCSS = readFileSync('./methods-public/styles.css');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    // Home
-    if (url === '/'){
-        res.writeHead(200, {'content-type':'text/html'});
-        res.end(homePage);
-    }
-    // Home/CSS
-    else if (url === '/styles.css'){
-        res.writeHead(200, {'content-type':'text/css'});
-        res.end(homeCSS);
-    }
-    // About
-    else if (url === '/about'){
-        res.writeHead(200, {'content-type':'text/html'});
-        res.end('<h1>About</h1>');
-    }
-    // 404
-    else {
-        res.writeHead(404, {'content-type':'text/html'});
-        res.end('<h1>Not Found</h1>');
-    }
+// 
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...');
 })
-
-server.listen(5000);
