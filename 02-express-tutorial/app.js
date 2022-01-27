692f7f783c6bdfb8918a72dfe9438e9a6e1ba1a6
Yonko Nikolov <jonkoslav@abv.bg>
2022-01-27 13:14:55 +0200

    Process basic GET requests

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,17 @@
-// change HTTPserver with Express
+// add get, add about, add 404
 
 const express = require('express');
 const app = express();
 
-// 
+app.get('/', (req, res) => {
+    res.status(200).send('Home Page');
+})
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page');
+})
+app.all('*', (req, res) => {
+    res.status(404).send('Not Found');
+})
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...');
