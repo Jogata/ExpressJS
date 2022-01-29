edf4bdfcbcabc290d6d9b14e9ce20166bb426455
Yonko Nikolov <jonkoslav@abv.bg>
2022-01-29 11:39:32 +0200

    Set index.html as static file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,21 @@
-// set index.html as Home page, add static files
+// set index.html as static file
 
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
+// Set Static Files
 app.use(express.static('./methods-public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './methods-public/index.html'));
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './methods-public/index.html'));
+// })
 app.get('/about', (req, res) => {
     res.send('About Page');
 })
 app.all('*', (req, res) => {
-    res.send('Not Found');
+    res.send('<h1>Not Found</h1>');
 })
 
 app.listen(5000, () => {
