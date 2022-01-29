389d15ff52a658ede5c1eb3b12513e8a909f3414
Jogata <53145312+Jogata@users.noreply.github.com>
2022-01-29 22:42:52 +0200

    add get for data.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,3 @@
-// add data.js
-
 const express = require('express');
 const products = require('./data');
 
@@ -9,14 +7,21 @@ const app = express();
 app.use(express.static('./methods-public'));
 
 app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+})
+
+app.get('api/products', (req, res) => {
     res.json(products);
 })
+
 app.get('/about', (req, res) => {
     res.send('About Page');
 })
+
 app.all('*', (req, res) => {
     res.send('Not Found');
 })
+
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...');
