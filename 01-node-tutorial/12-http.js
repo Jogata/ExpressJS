7cb456eea74886930ee8224fd985b2e9f070b5df
Jogata <53145312+Jogata@users.noreply.github.com>
2022-01-31 16:22:27 +0200

    Update 12-http.js
    Add v.2 for NodeJS Server without routing

--- 01-node-tutorial/12-http.js
+++ 01-node-tutorial/12-http.js
@@ -1,18 +1,18 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-
-  if (req.url === '/') {
-      res.end('Welcome to our home page');
-    }
-    if (req.url === '/about') {
-      res.end('Here is our short history');
-    }
-    res.end(`
-    <h1>Oops!</h1>
-      <p>We can't seem to find the page you are looking for</p>
-      <a href="/">back home</a>
-    `)
+// ====================   v.1   ====================
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//       res.end('Welcome to our home page');
+//     }
+//     if (req.url === '/about') {
+//       res.end('Here is our short history');
+//     }
+//     res.end(`
+//     <h1>Oops!</h1>
+//       <p>We can't find the page you are looking for</p>
+//       <a href="/">back home</a>
+//     `)
 
   // ###################################################
   //  IF YOU GET ERRORS WHILE USING ABOVE SETUP,
@@ -31,6 +31,19 @@ const server = http.createServer((req, res) => {
   //   <a href="/">back home</a>
   //   `)
   // }
+// })
+
+// ====================   v.2   ====================
+const server = http.createServer(function(req, res) {
+// Create Headers
+  res.setHeader('Content-type', 'text/html');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.writeHead(200);
+// Create Some Data for Response
+  const dataObj = {'id':123, 'name':'Jogata', 'email':'jogata@work.org'};
+// the response MUST be a string, so dataObj MUST be converted to string
+  const dataString = JSON.stringify(dataObj);
+  res.end(dataString);
 })
 
 server.listen(5000)
