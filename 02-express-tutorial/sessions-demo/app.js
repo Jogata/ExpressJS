92f8e23466c029451c101bff9fb3983cb4fc2e7c
Yonko Nikolov <jonkoslav@abv.bg>
2022-02-15 22:27:26 +0200

    set main, home, get login, get register routes

--- 02-express-tutorial/sessions-demo/app.js
+++ 02-express-tutorial/sessions-demo/app.js
@@ -4,6 +4,10 @@ const {users} = require('../data');
 
 const app = express();
 
+// BodyParser (middleware that give you access to the data from the body of the request)
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 app.use(session({
     name: 'sid',
     secret: 'This is a secret',
@@ -17,20 +21,63 @@ app.use(session({
     },
 }));
 
+const redirectLogin = (req, res, next) => {
+    if (req.session.userID) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
 app.get('/', (req, res) => {
-    res.send('/');
+    const { userID } = req.session;
+    res.send(`
+        <h1>Welcome!</h1>
+        ${userID ? `
+        <a href='/home'>Home</a>
+        <form method='post' action='/logout'>
+            <button>Logout</button>
+        </form>` : `
+        <a href='/login'>Login</a>
+        <a href='/register'>Register</a>
+        `} 
+    `)
 })
 
-app.get('/home', (req, res) => {
-    res.send('home');
+app.get('/home', redirectLogin, (req, res) => {
+    res.send(`
+    <h1>Welcome!</h1>
+    <a href='/'>Main</a>
+    <ul>
+        <li>Name: </li>
+        <li>ID: </li>
+    </ul>
+    `)
 })
 
 app.get('/login', (req, res) => {
-    res.send('get login');
+    res.send(`
+        <h1>Login</h1>
+        <form method='post' action='/login'>
+        <input type="text" name="name" id="name" placeholder="Name" required"/>
+        <input type="password" name="password" id="password" placeholder="Password" required"/>
+        <input type="submit"/>
+        </form>
+        <a href='/register'>Register</a>
+    `);
 })
 
 app.get('/register', (req, res) => {
-    res.send('get reg');
+    res.send(`
+        <h1>Register</h1>
+        <form method='post' action='/register'>
+        <input type="text" name="name" id="name" placeholder="Name" required"/>
+        <input type="email" name="email" id="email"/>
+        <input type="password" name="password" id="password" placeholder="Password" required"/>
+        <input type="submit"/>
+        </form>
+        <a href='/login'>Login</a>
+    `);
 })
 
 app.post('/login', (req, res) => {
